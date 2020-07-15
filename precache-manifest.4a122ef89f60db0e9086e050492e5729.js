77f4ec73f40b73754d0f22740c5ba226418a5efe
Wixes <dmitri.koma@yandex.ru>
2020-07-15 21:26:40 +0700

    Updates

---
+++ precache-manifest.4a122ef89f60db0e9086e050492e5729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c4abf699701d6176690e10ddfca723",
+    "url": "/abtestreal-client/index.html"
+  },
+  {
+    "revision": "9b79768763b1805e39f3",
+    "url": "/abtestreal-client/static/css/main.3685e888.chunk.css"
+  },
+  {
+    "revision": "d7b04c29698ac182187a",
+    "url": "/abtestreal-client/static/js/2.a2469df7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/abtestreal-client/static/js/2.a2469df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b79768763b1805e39f3",
+    "url": "/abtestreal-client/static/js/main.6c27b5ef.chunk.js"
+  },
+  {
+    "revision": "480640d42292f3a08f17",
+    "url": "/abtestreal-client/static/js/runtime-main.e612c562.js"
+  }
+]);
